1297f5ff1c699038ed3054ad90dac11fd7b422d8
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-04 15:39:54 +0900

    createメソッド定義

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,17 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+
+  def create
+    if params[:back]
+       render :new
+    else
+      if @blog.save
+        redirect_to new_blog_path, notice: 'ブログを作成しました。'
+      else
+        render :new
+      end
+    end
+  end
+  
 end
