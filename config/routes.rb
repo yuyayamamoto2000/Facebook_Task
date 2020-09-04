3700f06e84a8076191f98cd8ae058353b8525b6f
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-04 14:06:53 +0900

    sessionsのルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   resources :feeds
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
