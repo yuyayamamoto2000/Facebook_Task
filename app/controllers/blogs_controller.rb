afd97f2341304958edd35aab670d3c7792e4fb48
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-04 15:35:11 +0900

    新規投稿画面作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,5 @@
 class BlogsController < ApplicationController
+  def new
+    @blog = Blog.new
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_041841) do
+ActiveRecord::Schema.define(version: 2020_09_04_053700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+  end
 
   create_table "feeds", force: :cascade do |t|
     t.text "image"
@@ -27,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_041841) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
