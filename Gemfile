ca9da36a33f3952c1e36b8a024e745668afb686e
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-04 13:44:30 +0900

    パスワード設定

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.5'
 
+gem 'bcrypt', '3.1.11'
+
 gem 'carrierwave'
 gem 'mini_magick'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-   beffore_validation { email.downcase! }
+  beffore_validation { email.downcase! }
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
