07049e1df9cd99751a70f3eed03877e3d7a1f567
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-04 13:39:38 +0900

    emailを強制的に小文字化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+   beffore_validation { email.downcase! }
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
