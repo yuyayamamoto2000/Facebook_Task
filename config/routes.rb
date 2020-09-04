874c858cc22421c33c6b5c2040c6432354920665
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-04 13:54:45 +0900

    usersのルーティングにcreate追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :feeds
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
