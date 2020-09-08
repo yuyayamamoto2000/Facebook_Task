ae8cb985651c12f0b89c8b91ee19e7372c8b1cd9
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-08 10:45:04 +0900

    feedsのヘルパーファイル編集

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,2 +1,17 @@
 module FeedsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  def confirm_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_feeds_path
+    elsif action_name == 'edit' || action_name == 'update'
+      # render confirm_feed_path(@feed.id)
+      feed_path
+    end
+  end
+  def confirm_form_method
+    @feed.id ? 'patch' : 'post' # 作成時はpost, 編集時は patchを指定
+  end
+end
 end
