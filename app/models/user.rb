5781630c172e3231312c3bc68c54c55885385cb8
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-08 10:41:39 +0900

    user.rb編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-   has_many :blogs, dependent: :destroy
   before_validation { email.downcase! }
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  has_many :feeds
 end
