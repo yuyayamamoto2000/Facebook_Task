1d460956015ed3151585235c23cb8fe0f5b53a2f
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-08 10:29:10 +0900

    feedsとuser外部キー実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_012144) do
+ActiveRecord::Schema.define(version: 2020_09_08_012745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_09_08_012144) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "content"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_09_08_012144) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "feeds", "users"
 end
